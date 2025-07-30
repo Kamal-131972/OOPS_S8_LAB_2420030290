1b41dc7ff4377f4771db5c71ae0f54c4454df639
Kamal-131972 <dharmanakamalkumar@gmail.com>
2025-07-30 10:59:19 +0530

    Add files via upload

---
+++ OOPS_LAB/src/TEMPERATURE.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TEMPERATURE {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter the celsius :\t");
+			   int cel=sc.nextInt();
+			   int fah= cel*(9/5)+32;
+			   System.out.println("Converted temparature is :" +fah);
+		}
+
+	}
+
+}
---
+++ OOPS_LAB2/src/VOWEL_OR_NOT.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+public class VOWEL_OR_NOT {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter the alphabet\t");
+			   char al=sc.next().charAt(0);
+			   if(al=='a'||al=='e'||al=='i'||al=='o'||al=='u'||al=='A'||al=='E'||al=='I'||al=='O'||al=='U') {
+			     System.out.print("It is a Vowel\t");
+			   }
+			   else {
+			     System.out.print("It is a constants\t");
+			   }
+		 }
+
+		  }
+
+
+	}
