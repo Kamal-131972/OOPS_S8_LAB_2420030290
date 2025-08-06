630fc1035b63183a6f87fa1e2cec00174c9f63f7
Kamal-131972 <dharmanakamalkumar@gmail.com>
2025-08-06 10:19:21 +0530

    Add files via upload

---
+++ OOPS_TASK3/UserNameFormatter.java
@@ -0,0 +1,14 @@
+package lab3;
+
+public class UserNameFormatter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        String name = "  rAjEsh ";
+	        name.trim();
+	        name.toLowerCase();
+	        name.replace("a", "@");
+	        System.out.println("Formatted Name: " + name);
+	    }
+
+	}
